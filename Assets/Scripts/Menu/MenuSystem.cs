64129f4c7079dfc4cfc9701b3349d7d059e1539b
carolosano <114026092+carolosano@users.noreply.github.com>
2025-10-24 15:20:21 -0300

    cambios 24 oct
    agregue menu, actualice sprite vidas y mejore mecanica de ataque

---
+++ Assets/Scripts/Menu/MenuSystem.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSystem : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Saliendo del Juego...");
+        Application.Quit();
+    }
+}
